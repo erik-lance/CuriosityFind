b54d4b2fbc27d3c9423d26f224cbdf6e3ec7db91
erik-lance <eriklance@gmail.com>
2023-07-28 23:04:07 +0800

    Added database to services

--- Program.cs
+++ Program.cs
@@ -1,8 +1,14 @@
+using CuriosityFind.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
 
 var app = builder.Build();
 
