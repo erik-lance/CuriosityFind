d42151ba30573bbe17621a965b77e90c629ac08a
erik-lance <eriklance@gmail.com>
2023-07-28 22:27:42 +0800

    Wrapped theme over app

--- ClientApp/src/components/Layout.tsx
+++ ClientApp/src/components/Layout.tsx
@@ -1,5 +1,7 @@
+import { ThemeProvider } from '@mui/material';
 import React, { Component, ReactNode } from 'react';
 import { Container } from 'reactstrap';
+import theme from '../styles/theme';
 import { NavMenu } from './NavMenu';
 
 interface LayoutProps {
@@ -7,16 +9,18 @@ interface LayoutProps {
 }
 
 export class Layout extends Component<LayoutProps> {
-  static displayName = Layout.name;
+    static displayName = Layout.name;
 
-  render() {
-    return (
-      <div>
-        <NavMenu />
-        <Container>
-          {this.props.children}
-        </Container>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <ThemeProvider theme={theme}>
+                    <NavMenu />
+                    <Container>
+                        {this.props.children}
+                    </Container>
+                </ThemeProvider>
+            </div>
+        );
+    }
 }
