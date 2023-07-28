7a96e7863bb703df526f6c026d939529ed0e6303
erik-lance <eriklance@gmail.com>
2023-07-28 22:21:29 +0800

    Added initial color theme

---
+++ ClientApp/src/styles/theme.ts
@@ -0,0 +1,23 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            light: "#F4D06F",
+            main: "#FF8811",
+        },
+        secondary: {
+            main: "#9DD9D2"
+        },
+        background: {
+            default: "#FFF8F0",
+            paper: "#392F5A"
+        },
+        text: {
+            primary: "#000000",
+            secondary: "#000000",
+        },
+    },
+})
+
+export default theme;
