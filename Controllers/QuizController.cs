fc04d43de2f0a23748babd390a31f875e6f4d24d
erik-lance <eriklance@gmail.com>
2023-07-29 23:45:24 +0800

    Added initial CRUD for QuizController

--- Controllers/QuizController.cs
+++ Controllers/QuizController.cs
@@ -3,7 +3,9 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace CuriosityFind.Controllers
 {
-    public class QuizController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class QuizController : ControllerBase
     {
         private readonly ApplicationDbContext _context;
 
@@ -16,6 +18,69 @@ namespace CuriosityFind.Controllers
         {
             var quizzes = _context.Quizzes.ToList();
             return new JsonResult(quizzes);
+        }
+
+        public IActionResult GetAllQuizzes()
+        {
+            var quizzes = _context.Quizzes.ToList();
+            return Ok(quizzes);
+        }
+
+        public IActionResult GetQuiz(int id)
+        {
+            var quiz = _context.Quizzes.Find(id);
+            return Ok(quiz);
+        }
+
+        [HttpPost]
+        public IActionResult CreateQuiz([FromBody] Quiz quiz)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Quizzes.Add(quiz);
+            _context.SaveChanges();
+            return Ok(quiz);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateQuiz(int id, [FromBody] Quiz quiz)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var quizInDb = _context.Quizzes.Find(id);
+
+            if (quizInDb == null)
+            {
+                return NotFound();
+            }
+
+            quizInDb.Title = quiz.Title;
+            quizInDb.Description = quiz.Description;
+
+            _context.SaveChanges();
+            return Ok(quizInDb);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteQuiz(int id)
+        {
+            var quiz = _context.Quizzes.Find(id);
+            if (quiz == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                _context.Quizzes.Remove(quiz);
+                _context.SaveChanges();
+                return Ok(quiz);
+            }
         }
     }
 }
