99a97be53e1f7c637990e3ce6a60f523ca6e3472
erik-lance <eriklance@gmail.com>
2023-07-29 23:32:01 +0800

    Added context

--- Controllers/QuizController.cs
+++ Controllers/QuizController.cs
@@ -1,6 +1,14 @@
-﻿namespace CuriosityFind.Controllers
+﻿using CuriosityFind.Data;
+
+namespace CuriosityFind.Controllers
 {
     public class QuizController
     {
+        private readonly ApplicationDbContext _context;
+
+        public QuizController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
     }
 }
