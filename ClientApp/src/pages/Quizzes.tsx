572e81364178af5906aa36bbdc58d33155933e53
erik-lance <eriklance@gmail.com>
2023-07-30 01:02:35 +0800

    Added initial code to load quizzes

--- ClientApp/src/pages/Quizzes.tsx
+++ ClientApp/src/pages/Quizzes.tsx
@@ -1,10 +1,34 @@
 import React from "react";
-import { Container, Typography } from "@mui/material";
+import { useState } from "react";
+import { Container, List, ListItem, ListItemText, Typography } from "@mui/material";
 
 export default function Quizzes() {
+    const [quizzes, setQuizzes] = useState([]);
+
+    function fetchQuizzes() {
+        fetch("https://localhost:44482/api/quiz")
+            .then(response => response.json())
+            .then(data => setQuizzes(data));
+    }
+
+
     return (
         <Container>
             <Typography variant="h1">Quizzes</Typography>
+
+            <button onClick={fetchQuizzes}>Fetch Quizzes</button>
+
+            <List>
+                {quizzes.map(quiz => (
+                    <ListItem key="quiz.id">
+                        <ListItemText
+                            primary={quiz.title}
+                            secondary={quiz.description}
+                        />
+                    </ListItem>
+                ))}
+
+            </List>
         </Container>
     );
 }
