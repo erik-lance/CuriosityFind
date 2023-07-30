5493fe60abff1bdc89ad92f158f91c6992d9a632
erik-lance <eriklance@gmail.com>
2023-07-30 23:42:33 +0800

    Added question model

---
+++ Models/Question.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CuriosityFind.Models
+{
+    public class Question
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Question { get; set; }
+
+        [Required]
+        public string Answer { get; set; }
+    }
+}
