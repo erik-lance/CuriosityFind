fb580f29d67a35bf48b19398810d1226ed69c69f
erik-lance <eriklance@gmail.com>
2023-07-30 23:43:31 +0800

    Changed question variable to questiontext

--- Models/Question.cs
+++ Models/Question.cs
@@ -7,7 +7,7 @@ namespace CuriosityFind.Models
         public int Id { get; set; }
 
         [Required]
-        public string Question { get; set; }
+        public string QuestionText { get; set; }
 
         [Required]
         public string Answer { get; set; }
