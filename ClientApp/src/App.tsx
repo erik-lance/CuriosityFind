b88dffb8ea8abc3a27c1b4160a9b85793b0510fa
erik-lance <eriklance@gmail.com>
2023-07-31 14:28:24 +0800

    Added quiz page route

--- ClientApp/src/App.tsx
+++ ClientApp/src/App.tsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import AppRoutes from './AppRoutes';
 import { Layout } from './components/Layout';
 import './custom.css';
+import QuizPage from './pages/QuizPage';
 
 export default class App extends Component {
   static displayName = App.name;
@@ -15,6 +16,7 @@ export default class App extends Component {
             const { element, ...rest } = route;
             return <Route key={index} {...rest} element={element} />;
           })}
+          <Route path="/quiz/:id" element={<QuizPage />} />
         </Routes>
       </Layout>
     );
