ac53975d33403b607acb783eeba6a36ea6a64d0b
erik-lance <eriklance@gmail.com>
2023-07-31 13:33:45 +0800

    Added quiz seed

---
+++ Data/SeedData.cs
@@ -0,0 +1,26 @@
+﻿using CuriosityFind.Models;
+
+namespace CuriosityFind.Data
+{
+    public static class SeedData
+    {
+        public static void Initialize(ApplicationDbContext dbContext)
+        {
+            if (!dbContext.Quizzes.Any())
+            {
+                // Seed Quizzes
+                var quiz1 = QuizSeed.Quiz1();
+                var quiz2 = QuizSeed.Quiz2();
+
+                var quizzes = new List<Quiz>
+                {
+                    quiz1,
+                    quiz2
+                };
+
+                dbContext.AddRange(quizzes);
+                dbContext.SaveChanges();
+            }
+        }
+    }
+}
