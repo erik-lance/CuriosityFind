96c6a52e6c0116f791ff2e877035fe3b4150724e
erik-lance <eriklance@gmail.com>
2023-08-02 14:37:17 +0800

    Centered box contents

--- ClientApp/src/pages/QuizPage.tsx
+++ ClientApp/src/pages/QuizPage.tsx
@@ -1,4 +1,4 @@
-import { Button, CircularProgress, Container, Typography } from "@mui/material";
+import { Box, Button, CircularProgress, Container, Typography } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
@@ -35,8 +35,21 @@ function QuizPage() {
 
                         {/* Load Start, else load questions */}
                         {currentQuestion == 0 ? (<>
-                            <Typography variant="h3">Are you ready to begin?</Typography>
-                            <Button>Begin!</Button>
+                            <Box
+                                display="flex"
+                                flexDirection="column"
+                                gap={5}
+                                justifyContent="center"
+                                alignItems="center"
+                                minHeight="50vh"
+                            >
+                                <Typography variant="h3">Are you ready to begin?</Typography>
+                                <Button
+                                    variant="contained"
+                                >
+                                    Begin!
+                                </Button>
+                            </Box>
                         </>) : (<>
 
                             {/* If reached max question, else */}
