8742a331e5491fd62baa616def5537dcc7ddf080
erik-lance <eriklance@gmail.com>
2023-08-03 22:49:02 +0800

    Migrated Questions into DB

--- Migrations/20230803144502_Questions.Designer.cs
+++ Migrations/20230803144502_Questions.Designer.cs
@@ -2,6 +2,7 @@
 using CuriosityFind.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CuriosityFind.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230803144502_Questions")]
+    partial class Questions
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.9");
---
+++ Migrations/20230803144502_Questions.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CuriosityFind.Migrations
+{
+    /// <inheritdoc />
+    public partial class Questions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Question_Quizzes_QuizId",
+                table: "Question");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Question",
+                table: "Question");
+
+            migrationBuilder.RenameTable(
+                name: "Question",
+                newName: "Questions");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Question_QuizId",
+                table: "Questions",
+                newName: "IX_Questions_QuizId");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "QuizId",
+                table: "Questions",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "INTEGER",
+                oldNullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Questions",
+                table: "Questions",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Questions_Quizzes_QuizId",
+                table: "Questions",
+                column: "QuizId",
+                principalTable: "Quizzes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Questions_Quizzes_QuizId",
+                table: "Questions");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Questions",
+                table: "Questions");
+
+            migrationBuilder.RenameTable(
+                name: "Questions",
+                newName: "Question");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Questions_QuizId",
+                table: "Question",
+                newName: "IX_Question_QuizId");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "QuizId",
+                table: "Question",
+                type: "INTEGER",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Question",
+                table: "Question",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Question_Quizzes_QuizId",
+                table: "Question",
+                column: "QuizId",
+                principalTable: "Quizzes",
+                principalColumn: "Id");
+        }
+    }
+}
