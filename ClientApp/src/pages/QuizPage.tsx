26ccaa1264a577c4f5279e5225230170f6e52e22
erik-lance <eriklance@gmail.com>
2023-08-04 19:39:05 +0800

    Adjusted spacing of title

--- ClientApp/src/pages/QuizPage.tsx
+++ ClientApp/src/pages/QuizPage.tsx
@@ -35,31 +35,38 @@ function QuizPage() {
     return (
         <>
             <Container>
-                {!quiz && <CircularProgress />}
-                {quiz && (
-                    <>
-                        <Typography
-                            variant="h3"
-                        >
-                            {quiz.title}
-                        </Typography>
-
-                        <Typography
-                            variant="subtitle1"
-                        >
-                            {quiz.description}
-                        </Typography>
-                        
-                        {/* Load Start, else load questions */}
-                        {currentQuestion == -1 ? (<>
+                <Box
+                    display="flex"
+                    flexDirection="column"
+                    gap={5}
+                    justifyContent="center"
+                    alignItems="center"
+                    minHeight="50vh"
+                >
+                    {!quiz && <CircularProgress />}
+                    {quiz && (
+                        <>
                             <Box
-                                display="flex"
-                                flexDirection="column"
-                                gap={5}
-                                justifyContent="center"
-                                alignItems="center"
-                                minHeight="50vh"
+                                display="block"
+                                textAlign="left"
+                                minHeight="10vh"
+                                width="100%"
                             >
+                                <Typography
+                                    variant="h3"
+                                >
+                                    {quiz.title}
+                                </Typography>
+
+                                <Typography
+                                    variant="subtitle1"
+                                >
+                                    {quiz.description}
+                                </Typography>
+                            </Box>
+                            {/* Load Start, else load questions */}
+                            {currentQuestion == -1 ? (<>
+
                                 <Typography variant="h3">Are you ready to begin?</Typography>
                                 <Button
                                     variant="contained"
@@ -67,31 +74,32 @@ function QuizPage() {
                                 >
                                     Begin!
                                 </Button>
-                            </Box>
-                        </>) : (<>
 
-                            {/* If reached max question, else */}
-                            {currentQuestion === quiz.questions.length ? (<>
-                                <Typography variant="h3">You have completed the quiz!</Typography>
                             </>) : (<>
-                                {/* Displays each question one at a time */}
-                                {/* Question is based on questionnumber set */}
-                                <Typography
-                                    variant="h4"
-                                >
-                                    {quiz.questions[currentQuestion].questionTitle}
-                                </Typography>
 
-                                {/* Displays each answer for the question */}
+                                {/* If reached max question, else */}
+                                {currentQuestion === quiz.questions.length ? (<>
+                                    <Typography variant="h3">You have completed the quiz!</Typography>
+                                </>) : (<>
+                                    {/* Displays each question one at a time */}
+                                    {/* Question is based on questionnumber set */}
+                                    <Typography
+                                        variant="h4"
+                                    >
+                                        {quiz.questions[currentQuestion].questionTitle}
+                                    </Typography>
+
+                                    {/* Displays each answer for the question */}
+                                </>)}
                             </>)}
-                        </>)}
 
 
 
 
 
-                    </>
-                )}
+                        </>
+                    )}
+                </Box>
             </Container>
         </>
     );
