dbbb8427d7cc7c0390f4639c3a0b7731eb66b05a
erik-lance <eriklance@gmail.com>
2023-08-05 13:23:38 +0800

    Cleaned and optimized

--- ClientApp/src/pages/QuizPage.tsx
+++ ClientApp/src/pages/QuizPage.tsx
@@ -38,81 +38,75 @@ function QuizPage() {
             .catch(error => console.log(error));
     }, [id]);
 
-    return (
-        <>
-            <Container>
+    return (<Container>
+        <Box
+            display="flex"
+            flexDirection="column"
+            gap={5}
+            justifyContent="center"
+            alignItems="center"
+            minHeight="50vh"
+        >
+            {!quiz && <CircularProgress />}
+            {quiz && (<>
                 <Box
-                    display="flex"
-                    flexDirection="column"
-                    gap={5}
-                    justifyContent="center"
-                    alignItems="center"
-                    minHeight="50vh"
+                    display="block"
+                    textAlign="left"
+                    minHeight="10vh"
+                    width="100%"
                 >
-                    {!quiz && <CircularProgress />}
-                    {quiz && (
-                        <>
-                            <Box
-                                display="block"
-                                textAlign="left"
-                                minHeight="10vh"
-                                width="100%"
-                            >
-                                <Typography
-                                    variant="h3"
-                                >
-                                    {quiz.title}
-                                </Typography>
+                    <Typography
+                        variant="h3"
+                    >
+                        {quiz.title}
+                    </Typography>
 
-                                <Typography
-                                    variant="subtitle1"
-                                >
-                                    {quiz.description}
-                                </Typography>
-                            </Box>
-                            {/* Load Start, else load questions */}
-                            {currentQuestion == -1 ? (<>
-
-                                <Typography variant="h3">Are you ready to begin?</Typography>
-                                <Button
-                                    variant="contained"
-                                    onClick={() => setCurrentQuestion(0)}
-                                >
-                                    Begin!
-                                </Button>
-
-                            </>) : (<>
-                                {/* If reached max question, else */}
-                                {currentQuestion === quiz.questions.length ? (<>
-                                    <Typography variant="h3">You have completed the quiz!</Typography>
-                                </>) : (<>
-                                    {/* Displays each question one at a time */}
-                                    {/* Question is based on questionnumber set */}
-                                    <Typography
-                                        variant="h4"
-                                    >
-                                        {quiz.questions[currentQuestion].questionTitle}
-                                    </Typography>
-
-                                    {/* Displays each answer for the question */}
-                                            {console.log(quiz.questions[currentQuestion].options)}
-                                            {console.log(quiz.questions[currentQuestion]) }
-                                    {quiz.questions[currentQuestion].options.map(option => (
-                                        <Button
-                                            variant="contained"
-                                            onClick={() => nextQuestion()}
-                                        >
-                                            {option.optionText}
-                                        </Button>
-                                    ))}
-                                </>)}
-                            </>)}
-
-                        </>
-                    )}
+                    <Typography
+                        variant="subtitle1"
+                    >
+                        {quiz.description}
+                    </Typography>
                 </Box>
-            </Container>
-        </>
+                {/* Load Start, else load questions */}
+                {currentQuestion == -1 ? (<>
+
+                    <Typography variant="h3">Are you ready to begin?</Typography>
+                    <Button
+                        variant="contained"
+                        onClick={() => setCurrentQuestion(0)}
+                    >
+                        Begin!
+                    </Button>
+
+                </>) : (<>
+
+                    {/* If reached max question, else */}
+                    {currentQuestion === quiz.questions.length ? (
+                        <Typography variant="h3">You have completed the quiz!</Typography>
+                    ) : (<>
+
+                        {/* Displays each question one at a time */}
+                        {/* Question is based on questionnumber set */}
+                        <Typography
+                            variant="h4"
+                        >
+                            {quiz.questions[currentQuestion].questionTitle}
+                        </Typography>
+
+                        {/* Displays each answer for the question */}
+                        {quiz.questions[currentQuestion].options.map(option => (
+                            <Button
+                                variant="contained"
+                                onClick={() => nextQuestion()}
+                            >
+                                {option.optionText}
+                            </Button>
+                        ))}
+                    </>)}
+                </>)}
+            </>)}
+        </Box>
+    </Container>
     );
 }
 
