a3af7a1841561320b430988830cc5f041c8e0efe
erik-lance <eriklance@gmail.com>
2023-08-05 13:27:07 +0800

    Adjusted element spacings

--- ClientApp/src/pages/QuizPage.tsx
+++ ClientApp/src/pages/QuizPage.tsx
@@ -43,7 +43,7 @@ function QuizPage() {
             display="flex"
             flexDirection="column"
             gap={5}
-            justifyContent="center"
+            justifyContent="flex-start"
             alignItems="center"
             minHeight="50vh"
         >
@@ -52,8 +52,9 @@ function QuizPage() {
                 <Box
                     display="block"
                     textAlign="left"
-                    minHeight="10vh"
+                    height="15vh"
                     width="100%"
+                    paddingTop="5vh"
                 >
                     <Typography
                         variant="h3"
@@ -67,43 +68,50 @@ function QuizPage() {
                         {quiz.description}
                     </Typography>
                 </Box>
-                {/* Load Start, else load questions */}
-                {currentQuestion == -1 ? (<>
 
-                    <Typography variant="h3">Are you ready to begin?</Typography>
-                    <Button
-                        variant="contained"
-                        onClick={() => setCurrentQuestion(0)}
-                    >
-                        Begin!
-                    </Button>
+                <Box
+                    display="flex"
+                    flexDirection="column"
+                    gap={5}
+                    justifyContent="flex-start"
+                >
+                    {currentQuestion == -1 ? (<>
 
-                </>) : (<>
-
-                    {/* If reached max question, else */}
-                    {currentQuestion === quiz.questions.length ? (
-                        <Typography variant="h3">You have completed the quiz!</Typography>
-                    ) : (<>
-
-                        {/* Displays each question one at a time */}
-                        {/* Question is based on questionnumber set */}
-                        <Typography
-                            variant="h4"
+                        <Typography variant="h3">Are you ready to begin?</Typography>
+                        <Button
+                            variant="contained"
+                            onClick={() => setCurrentQuestion(0)}
                         >
-                            {quiz.questions[currentQuestion].questionTitle}
-                        </Typography>
+                            Begin!
+                        </Button>
 
-                        {/* Displays each answer for the question */}
-                        {quiz.questions[currentQuestion].options.map(option => (
-                            <Button
-                                variant="contained"
-                                onClick={() => nextQuestion()}
+                    </>) : (<>
+
+                        {/* If reached max question, else */}
+                        {currentQuestion === quiz.questions.length ? (
+                            <Typography variant="h3">You have completed the quiz!</Typography>
+                        ) : (<>
+
+                            {/* Displays each question one at a time */}
+                            {/* Question is based on questionnumber set */}
+                            <Typography
+                                variant="h4"
                             >
-                                {option.optionText}
-                            </Button>
-                        ))}
+                                {quiz.questions[currentQuestion].questionTitle}
+                            </Typography>
+
+                            {/* Displays each answer for the question */}
+                            {quiz.questions[currentQuestion].options.map(option => (
+                                <Button
+                                    variant="contained"
+                                    onClick={() => nextQuestion()}
+                                >
+                                    {option.optionText}
+                                </Button>
+                            ))}
+                        </>)}
                     </>)}
-                </>)}
+                </Box>
             </>)}
         </Box>
     </Container>
