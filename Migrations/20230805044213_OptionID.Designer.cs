f998dd3a150607b263681dd07927a81cc58366de
erik-lance <eriklance@gmail.com>
2023-08-05 12:42:35 +0800

    Migrated DB update to include QuestionId for Option

--- Migrations/20230805044213_OptionID.Designer.cs
+++ Migrations/20230805044213_OptionID.Designer.cs
@@ -2,6 +2,7 @@
 using CuriosityFind.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CuriosityFind.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230805044213_OptionID")]
+    partial class OptionID
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.9");
---
+++ Migrations/20230805044213_OptionID.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CuriosityFind.Migrations
+{
+    /// <inheritdoc />
+    public partial class OptionID : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Options_Questions_QuestionId",
+                table: "Options");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "QuestionId",
+                table: "Options",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "INTEGER",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Options_Questions_QuestionId",
+                table: "Options",
+                column: "QuestionId",
+                principalTable: "Questions",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Options_Questions_QuestionId",
+                table: "Options");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "QuestionId",
+                table: "Options",
+                type: "INTEGER",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Options_Questions_QuestionId",
+                table: "Options",
+                column: "QuestionId",
+                principalTable: "Questions",
+                principalColumn: "Id");
+        }
+    }
+}
