2b4000ca50ff51f0b7c584a2a184251da77abb78
erik-lance <eriklance@gmail.com>
2023-08-05 11:44:59 +0800

    Added new option class

---
+++ Models/Option.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CuriosityFind.Models
+{
+    public class Option
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string OptionText { get; set; } = string.Empty;
+    }
+}
