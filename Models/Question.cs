fdf564b7463fe66e1b3554673879c9cf0366b43c
erik-lance <eriklance@gmail.com>
2023-08-05 11:17:37 +0800

    Added choices to question db

--- Models/Question.cs
+++ Models/Question.cs
@@ -10,6 +10,9 @@ namespace CuriosityFind.Models
         public string QuestionTitle { get; set; } = string.Empty;
 
         [Required]
+        public string[] Choices { get; set; } = new string[4];
+
+        [Required]
         public string Answer { get; set; } = string.Empty;
 
         public int QuizId { get; set; }
